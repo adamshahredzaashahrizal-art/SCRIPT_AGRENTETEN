277a27192f50992f21a6c307f79f8cc20ba74293
adamshahredzaashahrizal-art <adamshahredzaashahrizal@gmail.com>
2026-08-09 21:08:20 +0800

    Add files via upload

--- settings/config.js
+++ settings/config.js
@@ -1,1 +1,8 @@
-
+global.prefa = ['','!','.',',','🐤','🗿']
+global.owner = ['62']
+global.packname = "Sticker By Denny"
+global.author = "DennyOfficial"
+global.mess = {
+ owner: '*You are not the owner*',
+ premium: '*You are not premium*'
+}
